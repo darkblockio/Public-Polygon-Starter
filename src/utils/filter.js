36f326a918e2478871b44dd92f325d9873a06939
Gary Strader <gstrader28@yahoo.com>
2022-09-29 10:15:09 -0500

    make sure nft filtered list is comparing lower contract and token ids.

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,7 +2,7 @@ export const filter = (array, apiData) => {
   let filterData = []
   array.forEach((nft) => {
     apiData.data.filter((item) => {
-      if (item.contract === nft.contract && item.token === nft.token) {
+      if (item.contract.toLowerCase() === nft.contract.toLowerCase() && item.token.toLowerCase() === nft.token.toLowerCase()) {
         filterData.push(item)
       }
     })
